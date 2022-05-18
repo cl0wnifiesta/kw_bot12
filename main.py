fc07671429f5d96331eb07b494be7f39208a8a20
sdlfkgfja12 <90916902+sdlfkgfja12@users.noreply.github.com>
2022-05-18 16:32:00 +0300

    #3

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import aiogram
+from aiogram.utils.executor import start_webhook
 
 from config import dp, bot
 from aiogram import executor
 from db_manager import DatabaseManager
 import middlewares
 import handlers
-import cherrypy
 
 
 WEBHOOK_HOST = '109.234.34.41'
@@ -17,38 +17,22 @@ WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному кл
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % '5365166446:AAEe740Q5yPT2IlHdsFvKACr9xSH6ASN8xk'
 
-# class WebhookServer(object):
-#     @cherrypy.expose
-#     def index(self):
-#         if 'content-length' in cherrypy.request.headers and \
-#                         'content-type' in cherrypy.request.headers and \
-#                         cherrypy.request.headers['content-type'] == 'application/json':
-#             length = int(cherrypy.request.headers['content-length'])
-#             json_string = cherrypy.request.body.read(length).decode("utf-8")
-#             update = aiogram.types.Update.as_json(json_string)
-#             # Эта функция обеспечивает проверку входящего сообщения
-#             await bot.get_updates([update])
-#             return ''
-#         else:
-#             raise cherrypy.HTTPError(403)
-#
-# cherrypy.config.update({
-#     'server.socket_host': WEBHOOK_LISTEN,
-#     'server.socket_port': WEBHOOK_PORT,
-#     'server.ssl_module': 'builtin',
-#     'server.ssl_certificate': WEBHOOK_SSL_CERT,
-#     'server.ssl_private_key': WEBHOOK_SSL_PRIV
-# })
 
 async def on_startup(_):
     await DatabaseManager().create_tables()
+    await bot.set_webhook(url=WEBHOOK_URL_BASE, certificate=open(WEBHOOK_SSL_CERT, 'r'))
     print("Бот онлайн")
+
+async def on_shutdown(_):
+    await bot.delete_webhook()
 
 if __name__ == "__main__":
     middlewares.setup(dp)
-    await bot.delete_webhook()
-    await bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-                          certificate=open(WEBHOOK_SSL_CERT, 'r'))
-    await on_startup("")
-    # executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
-
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_URL_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host='localhost',
+        port=3001)
