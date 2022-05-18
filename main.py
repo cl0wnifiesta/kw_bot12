2f71c937ae80be019033d7ae4e74d924466c11ad
sdlfkgfja12 <90916902+sdlfkgfja12@users.noreply.github.com>
2022-05-18 21:51:13 +0300

    #8

--- main.py
+++ main.py
@@ -3,30 +3,21 @@ from aiogram.utils.executor import start_webhook
 from config import dp, bot
 from aiogram import executor
 from db_manager import DatabaseManager
-import middlewares
-import handlers
+import handlers, middlewares, keyboards, states, utils
 
-WEBHOOK_HOST = 'https://e7f1-109-234-34-41.eu.ngrok.io' # сюда ссылку из Ngrok
+WEBHOOK_HOST = 'https://e7f1-109-234-34-41.eu.ngrok.io'
 WEBHOOK_PATH = ''
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-# webserver settings
-WEBAPP_HOST = 'localhost'  # or ip
+WEBAPP_HOST = 'localhost'
 WEBAPP_PORT = 80
 
-
-async def on_startup(_):
+async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
 
 
-async def on_shutdown(_):
-
-    # insert code here to run it before shutdown
-
-    # Remove webhook (not acceptable in some cases)
+async def on_shutdown(dp):
     await bot.delete_webhook()
-
-    # Close DB connection (if used)
     await dp.storage.close()
     await dp.storage.wait_closed()
 
