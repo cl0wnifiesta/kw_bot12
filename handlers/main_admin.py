3f6de2715c97f329140435af7f6643b904989a82
sdlfkgfja12 <90916902+sdlfkgfja12@users.noreply.github.com>
2022-05-20 20:28:49 +0300

    fix#101

--- handlers/main_admin.py
+++ handlers/main_admin.py
@@ -10,15 +10,18 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 
 from keyboards.admin.admin_kb import main_admin_kb
+from keyboards.client.main_keyboard import main_kb
 
 
 @dp.message_handler(commands="admin", user_id=admins)
 async def admin_main(message: types.Message):
     await message.answer("Меню админ-панели", reply_markup=main_admin_kb)
 
-@dp.message_handler(Text(equals="↩Вернуться в режим пользователя"), state=None, user_id=admins)
-async def get_back_to_user(message: types.Message):
-    await start_message(message)
+@dp.message_handler(Text(equals="↩Вернуться в режим пользователя"), user_id=admins)
+async def get_back_to_user(message: types.Message, state: FSMContext):
+    await bot.send_message(message.from_user.id, "Добро пожаловать в магазин 🔥 фарм аккаунтов по приемлемым ценам 💵. "
+                                                 "Продажа аккаунтов только в одни руки 🤲.", reply_markup=main_kb)
+    await state.finish()
 
 @dp.callback_query_handler(Text("adm_close"))
 async def admin_close(call: types.CallbackQuery, state: FSMContext):
